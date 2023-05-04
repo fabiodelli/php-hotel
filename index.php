36ae8b5ecacc94074c5a45edaa2ee6f94e503fb4
fabiodelli <ntqdel@hotmail.com>
2023-05-05 00:49:26 +0200

    modify table

--- index.php
+++ index.php
@@ -4,35 +4,35 @@
 
         [
             'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere ',
+            'description' => 'Hotel Belvedere Descrizione ',
             'parking' => true,
             'vote' => 4,
             'distance_to_center' => 10.4
         ],
         [
             'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro ',
+            'description' => 'Hotel Futuro Descrizione ',
             'parking' => true,
             'vote' => 2,
             'distance_to_center' => 2
         ],
         [
             'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare ',
+            'description' => 'Hotel Rivamare Descrizione ',
             'parking' => false,
             'vote' => 1,
             'distance_to_center' => 1
         ],
         [
             'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista ',
+            'description' => 'Hotel Bellavista Descrizione ',
             'parking' => false,
             'vote' => 5,
             'distance_to_center' => 5.5
         ],
         [
             'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano ',
+            'description' => 'Hotel Milano Descrizione ',
             'parking' => true,
             'vote' => 2,
             'distance_to_center' => 50
@@ -82,7 +82,6 @@
     else {
       echo "<td><i class='fa-solid fa-check'></i></td>";
     }
-    echo "<td>{$hotel['parking']}</td>";
     echo "<td>{$hotel['vote']}</td>";
     echo "<td>{$hotel['distance_to_center']}</td>";
     echo "</tr>";
